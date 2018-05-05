ff2a4ffd392f9acc306041f855a9f2bcefec59af
niubilitisong <66543775xie>
2018-05-05 13:42:05 +0800

    finish change and find person test

--- src/test/java/springboot/springboot/SpringbootApplicationTests.java
+++ src/test/java/springboot/springboot/SpringbootApplicationTests.java
@@ -9,10 +9,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.util.HashMap;
-import java.util.Map;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -60,5 +57,40 @@ public class SpringbootApplicationTests {
         mvc.perform(request).andExpect(content().string(equalTo("delete success!")));
     }
 
+    @Test
+    public void testFindPerson_should_return_person() throws Exception{
+        request = post("/employees/add")
+                .param("id","1")
+                .param("name","谢嵩")
+                .param("age","20")
+                .param("gender","男");
+        mvc.perform(request);
 
+        request = post("/employees/findperson")
+                .param("id","1");
+        mvc.perform(request).andExpect(content().string(equalTo("{\"id\":1,\"name\":\"谢嵩\",\"age\":20,\"gender\":\"男\"}")));
+
+    }
+
+    @Test
+    public void testChangePerson_should_return_another_person() throws Exception{
+
+        request = post("/employees/add")
+                .param("id","1")
+                .param("name","谢嵩")
+                .param("age","20")
+                .param("gender","男");
+        mvc.perform(request);
+
+        request = post("/employees/add")
+                .param("id","1")
+                .param("name","张三")
+                .param("age","22")
+                .param("gender","女");
+        mvc.perform(request);
+
+        request = post("/employees/findperson")
+                .param("id","1");
+        mvc.perform(request).andExpect(content().string(equalTo("{\"id\":1,\"name\":\"张三\",\"age\":22,\"gender\":\"女\"}")));
+    }
 }
